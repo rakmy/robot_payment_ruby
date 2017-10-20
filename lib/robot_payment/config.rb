38e727da60a725ef34a76870e298c250fd5e6017
mika <mika.mizuno@gmail.com>
2017-10-20 16:27:52 +0900

    Enable config in rails app

---
+++ lib/robot_payment/config.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module RobotPayment
+  # Configures global settings for RobotPayment
+  #   RobotPayment.configure do |config|
+  #     config.aid = 123456
+  #   end
+  class << self
+    def configure
+      yield config
+    end
+
+    def config
+      @_config ||= Config.new
+    end
+  end
+
+  class Config
+    attr_accessor :aid, :rt, :max_amount, :min_amount
+    def initialize
+      @aid = ENV["AID"]
+      @rt = 0
+      @max_amount = 1000
+      @min_amount = 100_000
+    end
+  end
+end
