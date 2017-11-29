332801e117e2cab5424e0401ed1a820cce631bbd
mika <mika.mizuno@gmail.com>
2017-11-29 14:42:15 +0900

    Modify paths, Imprement pause

---
+++ lib/robot_payment/pause.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require "faraday"
+require "json"
+
+module RobotPayment
+  class Pause
+
+    def pause_url
+      RobotPayment.pause_url
+    end
+
+    def pause(params)
+      client = Faraday.new(url: pause_url)
+      charge = RobotPayment::Pause.new
+      url = charge.query_builder(params)
+      client.post url
+    end
+
+    def query_builder(params)
+      "#{pause_url}?#{basic_params(params)}"
+    end
+
+    def basic_params(params)
+      q = {
+        aid: RobotPayment.config.aid,
+        cmd: 1                       # 変更時に1を指定
+        }
+      q.merge!(params) if params
+      q = q.map{|key,val| "#{key}=#{val}"}.join("&")
+    end
+  end
+end
