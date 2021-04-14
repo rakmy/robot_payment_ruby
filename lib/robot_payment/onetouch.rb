372605eec68b767fbbf9d77426586a78d2457caf
naoyanickf <naoyanickf@gmail.com>
2021-04-14 16:56:52 +0900

    add onetouch payment

---
+++ lib/robot_payment/onetouch.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require "faraday"
+require "json"
+
+module RobotPayment
+  class Onetouch
+    def onetouch_url
+      RobotPayment.onetouch_url
+    end
+
+    def create(params)
+      client = Faraday.new(url: onetouch_url)
+      charge = RobotPayment::Onetouch.new
+      url = charge.query_builder(params)
+      client.post url
+    end
+
+    def query_builder(params)
+      "#{onetouch_url}?#{basic_params(params)}"
+    end
+
+    def basic_params(params)
+      q = {
+        aid: RobotPayment.config.aid,
+        rt: 1,
+        cmd: 2,
+        jb: "CAPTURE", # 店舗ID・店舗オーダー番号
+        }
+      q.merge!(params) if params
+      q = q.map{|key,val| "#{key}=#{val}"}.join("&")
+    end
+  end
+end
